cff6612fbd1d30fc250f0de12a7cb033af8fd7ea
Tom-Hansen <143756451+Tom-Hansen@users.noreply.github.com>
2023-10-06 16:32:45 -0400

    Add files via upload
    questions 1 and 2

---
+++ Questions1&2.m
@@ -0,0 +1,59 @@
+#include "HW-1.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+
+void question1() {
+    double L_p = -1;
+    double L_delta = 1;
+    double deflect = 1;
+    double p = 1; // rad/s. This is p0 initially
+    double p_old = 1;
+    double delta_t = 0.01;
+
+    for (double i = 0; i <= 5; i = (i + delta_t)) {
+        if (i > 2) {
+            deflect = 0;
+        }
+        if (i <= 2) {
+            deflect = 1;
+        }
+
+        p = p_old + (((L_p * p_old) + (L_delta * deflect)) * delta_t);
+
+        cout << "Time = " << i << " seconds and roll rate = " << p << " rad/s." << endl;
+
+        p_old = p;
+    }
+}
+
+void question2() {
+    cout << "QUESTION 2:" << endl;
+    int K = 2; // Control gain
+
+    // Resetting the previous variables for a new loop
+    double L_p = -1;
+    double L_delta = 1;
+    double p = 0;
+    double p_old = 0;
+    double deflect = -K * p_old; // Redefining the deflection formula
+    double delta_t = 0.01;
+
+    for (double i = 0; i <= 5; i = (i + delta_t)) {
+        if (i > 2) {
+            deflect = 0;
+        }
+        if (i <= 2) {
+            deflect = 1;
+        }
+
+        p = p_old + (((L_p * p_old) + (L_delta * deflect)) * delta_t);
+
+        cout << "Time = " << i << " seconds and roll rate = " << p << " rad/s." << endl;
+
+        p_old = p;
+    }
+}
