c8b7ace7251b22a078b678f3ed6f5eff74cf13d9
Tom-Hansen <143756451+Tom-Hansen@users.noreply.github.com>
2023-10-06 18:07:30 -0400

    Create Aircraft values (x).cpp

---
+++ Aircraft values (x).cpp
@@ -0,0 +1,34 @@
+
+// This is the data for the speed, wingspan, and Maximum take-off weight of aircraft models and their given engine types
+
+  // M-346 Master
+  x = [124, 31.89, 20.945]; //Speed, Wingspan, MTOW
+  y = [1]; //Turbojet
+
+  // AT-402B
+  x = [74, 51.08, 9.170];
+  y = [0]; //Turboprop
+
+  // MB-326
+  x = [103, 34.67, 8.300]
+  y = [1]; //Turbojet
+
+  //AT-502B
+  x = [77, 52.00, 9.400];
+  y = [0]; //Turboprop  
+
+  //MB-339
+  x = [104, 35.63, 13.000];
+  y = [1]; //Turbojet
+
+  //AT-602
+  x = [92, 56.00, 9.400];
+  y = [0]; //Turboprop  
+
+  //Aero L-159 Alca
+  x = [130, 31.29, 17.637];
+  y = [1]; //Turbojet
+
+  //AT-504
+  x = [73, 52.00, 9.600];
+  y = [0]; //Turboprop 
