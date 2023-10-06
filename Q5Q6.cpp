a2b921f150d796a451d06ea34792d2073a490c26
KMMetcalf <143756433+KMMetcalf@users.noreply.github.com>
2023-10-06 17:52:23 -0400

    Update Q5Q6.cpp

--- Q5Q6.cpp
+++ Q5Q6.cpp
@@ -2,14 +2,15 @@
 // Sigmoid equation for #5
 double sigmoid(double z)
 {
-    //Formula
+    //Formula for sigmoid
     double sigma_z = (1.0 / (1.0 + exp(-z)));
     return sigma_z;
 }
-
+//function for gradient_sigmoid
 double gradient_sigmoid(double z)
 {
     double sigma_z = sigmoid(z);
+    //formula for gradient sigmoid
     double d_sigma = (sigma_z * (1.0 - sigma_z));
     return d_sigma;
 }
@@ -19,16 +20,17 @@ void Q5()
     cout << "QUESTION 5:" << endl;
     //Variables
     double z = 0.0176835;
-    double sigmoid_output = sigmoid(z);
-    double gradient_output = gradient_sigmoid(z);
+    double sigmoid_output = sigmoid(z); //sigmoid output value
+    double gradient_output = gradient_sigmoid(z); //gradient sigmoid output value
 //Displaying answer to question
     cout << "Sigmoid(" << z << ") = " << sigmoid_output << std::endl;
     cout << "Gradient of the sigmoid(" << z << ") = " << gradient_output << std::endl;
 }
 //Question 6
+//function for gradient cost
 double gradient_cost(double y, double y_predict)
 {
-    //Equation
+    //formula for gradient cost
     double dC = 2 * (y_predict - y);
     return dC;
 }
@@ -38,9 +40,9 @@ void Q6()
     //Display
     cout << "QUESTION 6:" << endl;
     //Variables
-    double y = 1.0;
-    double y_predicted = 0.504421;
+    double y = 1.0; //variable y
+    double y_predicted = 0.504421; //y predicted
     double dC = gradient_cost(y_predicted, y);
-//Displaying
+//Displaying gradient cost value
     cout << "dC = " << dC << std::endl;
 }
