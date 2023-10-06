959f42927bc2792c1ece96588eedd00f26b45250
KMMetcalf <143756433+KMMetcalf@users.noreply.github.com>
2023-10-06 17:33:15 -0400

    Update and rename Homework1.cpp to Q5Q6.cpp

--- Q5Q6.cpp
+++ Q5Q6.cpp
@@ -1,6 +1,8 @@
-﻿// Sigmoid equation for #5
+Question 5
+// Sigmoid equation for #5
 double sigmoid(double z)
 {
+    //Formula
     double sigma_z = (1.0 / (1.0 + exp(-z)));
     return sigma_z;
 }
@@ -15,28 +17,30 @@ double gradient_sigmoid(double z)
 void Q5()
 {
     cout << "QUESTION 5:" << endl;
+    //Variables
     double z = 0.0176835;
-
     double sigmoid_output = sigmoid(z);
     double gradient_output = gradient_sigmoid(z);
-
+//Displaying answer to question
     cout << "Sigmoid(" << z << ") = " << sigmoid_output << std::endl;
     cout << "Gradient of the sigmoid(" << z << ") = " << gradient_output << std::endl;
 }
-
+//Question 6
 double gradient_cost(double y, double y_predict)
 {
+    //Equation
     double dC = 2 * (y_predict - y);
     return dC;
 }
 
 void Q6()
 {
+    //Display
     cout << "QUESTION 6:" << endl;
+    //Variables
     double y = 1.0;
     double y_predicted = 0.504421;
-
     double dC = gradient_cost(y_predicted, y);
-
+//Displaying
     cout << "dC = " << dC << std::endl;
 }
